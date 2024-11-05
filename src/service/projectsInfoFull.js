a15aeea418fe87536f7082d4bdcbd04e485c7039
Moises Sanchez <moisesanchezwork@gmail.com>
2024-11-05 13:02:09 -0400

    links & img updated

--- src/service/projectsInfoFull.js
+++ src/service/projectsInfoFull.js
@@ -3,17 +3,17 @@ export const PROJECTS = [
 	{
 		title: "Hit The Light",
 		description: "A fun game where you have to touch the square that lights up.",
-		link: "",
-		github: "",
-		image: "/photos/project.webp",
+		link: "#",
+		github: "https://github.com/monchigit/Hit-the-light",
+		image: "/photos/hit-the-light.webp",
 		tags: [TAGS.REACT, TAGS.HTML, TAGS.CSS, TAGS.JS]
 	},
 	{
 		title: "My Portfolio",
 		description: "A portfolio with my skills, projects and the benefits of hire me.",
-		link: "",
-		github: "",
-		image: "/photos/project.webp",
+		link: "#",
+		github: "https://github.com/monchigit/portfolio-monchi",
+		image: "/photos/portfolio.webp",
 		tags: [TAGS.HTML, TAGS.ASTRO, TAGS.TAILWIND, TAGS.REACT]
 	},
 ]
